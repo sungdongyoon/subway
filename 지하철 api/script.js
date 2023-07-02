867b3bd059157a77213dbe7236432b9a969bc1cd
sungdongyoon <ehdtjd0906@naver.com>
2023-07-02 22:12:17 +0900

    07/02 22:12

--- 지하철 api/script.js
+++ 지하철 api/script.js
@@ -1,54 +1,73 @@
-const getLiveData = async () => {
-  let res = await fetch(`http://swopenAPI.seoul.go.kr/api/subway/45724870526568643434635358536e/json/realtimePosition/0/20/1호선`);
-  let data = await res.json();
+// const getLiveData = async () => {
+//   let res = await fetch(`http://swopenAPI.seoul.go.kr/api/subway/45724870526568643434635358536e/json/realtimePosition/0/20/1호선`);
+//   let data = await res.json();
 
-  function subwayState() {
-    let state = data.realtimePositionList[0].trainSttus;
-    if(state === 0) {
-      state.innerText = "진입";
-    }
-    if(state === 1) {
-      state.innerText = "도착";
-    }
-    if(state === 2) {
-      state.innerText = "출발";
-    }
-    if(state === 3) {
-      state.innerText = "전역출발";
-    }
-  }
-  subwayState();
-  console.log(data);
-  console.log(data.realtimePositionList[0]);
-  console.log(`${data.realtimePositionList[0].subwayNm},
-  현위치: ${data.realtimePositionList[0].statnNm}역,
-  ${data.realtimePositionList[0].trainNo}번 열차,
-  상하행선: ${data.realtimePositionList[0].updnLine},
-  ${data.realtimePositionList[0].statnTnm}행,
-  상태: ${data.realtimePositionList[0].trainSttus}`);
+//   let state = data.realtimePositionList[0].trainSttus;
+//   let direction = data.realtimePositionList[0].updnLine;
+//   function subwayDirection() {
+//     if(direction == 0) {
+//       direction = "상";
+//     }
+//     if(direction == 1) {
+//       direction = "하";
+//     }
+//   }
+//   function subwayState() {
+//     if(state == 0) {
+//       state = "진입";
+//     }
+//     if(state == 1) {
+//       state = "도착";
+//     }
+//     if(state == 2) {
+//       state = "출발";
+//     }
+//     if(state == 1) {
+//       state = "전역출발";
+//     }
+//   }
+//   subwayDirection();
+//   subwayState();
+//   // console.log(data);
+//   // console.log(data.realtimePositionList[0]);
 
-  const subwayLine = document.querySelector(".subway_line");
-  const subwayUpdnLine = document.querySelector(".subway_updnLine");
-  const subwayNum = document.querySelector(".subway_num");
-  const subwayTerminus = document.querySelector(".subway_terminus");
-  const currentLocation = document.querySelector(".current_location");
+//   const subwayLine = document.querySelector(".subway_line");
+//   const subwayUpdnLine = document.querySelector(".subway_updnLine");
+//   const subwayNum = document.querySelector(".subway_num");
+//   const subwayTerminus = document.querySelector(".subway_terminus");
+//   const currentLocation = document.querySelector(".current_location");
 
-  subwayLine.innerText = `${data.realtimePositionList[0].subwayNm}`;
-  subwayUpdnLine.innerText = `${data.realtimePositionList[0].updnLine}행선`;
-  subwayNum.innerText = `${data.realtimePositionList[0].trainNo}번 열차`;
-  subwayTerminus.innerText = `${data.realtimePositionList[0].statnTnm}행`;
-  currentLocation.innerText = `현위치 : ${data.realtimePositionList[0].statnNm}역 ${data.realtimePositionList[0].trainSttus}`;
-}
-
-getLiveData()
-
-
-
-
-// const getStationData = async () => {
-//   let res = await fetch(`http://swopenAPI.seoul.go.kr/api/subway/45724870526568643434635358536e/json/realtimeStationArrival/0/5/서울`)
-//   let data = await res.json()
-//   console.log(data)
+//   subwayLine.innerText = `${data.realtimePositionList[0].subwayNm}`;
+//   subwayUpdnLine.innerText = `${direction}행선`;
+//   subwayNum.innerText = `${data.realtimePositionList[0].trainNo}번 열차`;
+//   subwayTerminus.innerText = `${data.realtimePositionList[0].statnTnm}행`;
+//   currentLocation.innerText = `현위치 : ${data.realtimePositionList[0].statnNm}역 ${state}`;
 // }
 
-// getStationData()
+// getLiveData()
+
+
+
+
+
+
+
+const getStationData = async () => {
+  let res = await fetch(`http://swopenAPI.seoul.go.kr/api/subway/45724870526568643434635358536e/json/realtimeStationArrival/0/10/회룡`)
+  let data = await res.json();
+  console.log(data);
+  
+  const stationName = document.querySelector(".station_name");
+  const destination1 = document.querySelector(".destination1");
+  const destination2 = document.querySelector(".destination2");
+  const stationInfo1 = data.realtimeArrivalList[4];
+  const stationInfo2 = data.realtimeArrivalList[5];
+  
+  stationName.innerHTML = `<span>${stationInfo1.statnNm}역 도착정보</span>`;
+
+  destination1.innerHTML = `<div>1</div><span><b>${stationInfo1.trainLineNm}</span></b> <br/> <span>${stationInfo1.arvlMsg2}</span> <br/>
+  ${stationInfo1.barvlDt}초 후에 도착 예정입니다.`;
+
+  destination2.innerHTML = `<div>2</div><span><b>${stationInfo2.trainLineNm}</span></b> <br/> <span>${stationInfo2.arvlMsg2}</span> <br/>
+  ${stationInfo2.barvlDt}초 후에 도착 예정입니다.`;
+}
